97e3d86c452e100d08c46da414a51ec942b2cee1
nikvxo <nikvonessen34@gmail.com>
2025-07-06 16:25:05 -0400

    Additional core logic added.

--- script.js
+++ script.js
@@ -13,3 +13,26 @@ function addBookToLibrary(title, author, pages, read) {
     const newBook = new Book(title, author, pages, read, id);
     myLibrary.push(newBook);
 }
+
+function displayBooks() {
+    const container = document.getElementById('booksContainer');
+    container.innerHTML = '';
+
+    myLibrary.forEach(book => {
+        const bookDiv = document.createElement('div');
+        bookDiv.className = 'book';
+        bookDiv.innerHTML = `
+            <h3>${book.title}</h3>
+            <p>Author: ${book.author}</p>
+            <p>Pages: ${book.pages}</p>
+            <p>Read: ${book.read ? 'Yes' : 'No'}</p>
+        `;
+        container.appendChild(bookDiv);
+    });
+}
+
+document.getElementById('newBookBtn').addEventListener('click', () => {
+    document.getElementById('newBookForm').style.display = 'block';
+})
+
+displayBooks();
